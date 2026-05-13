60ef3e0e035c341482c528bc5531c64e37fc05c6
zohaib68 <zohaibsarwar712@gmail.com>
2026-05-13 16:50:59 +0500

    fix: added mongodburi connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // Load environment variables
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // Connect MongoDB
+    MongooseModule.forRoot(process.env.MONGODB_URI as string),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }

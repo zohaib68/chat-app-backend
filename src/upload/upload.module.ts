71fd530b61cda6a0f898eea41cf8ea44dd7066c5
zohaib68 <zohaibsarwar712@gmail.com>
2026-05-14 17:52:35 +0500

    fix: fixed circular modue dependcy issue

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
 import { SupabaseService } from '../supabase/supabase.service';
+import { AuthModule } from '../auth/auth.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
+    imports: [AuthModule, UsersModule],
     controllers: [UploadController],
     providers: [UploadService, SupabaseService],
     exports: [UploadService],
 })
-export class UploadModule { }
+export class UploadModule { }
